f354ded00ef15174839906325d75001db69741b6
Jamar Graves <jamargraves07@gmail.com>
2024-02-24 15:24:21 -0500

    ezpz

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "value";
+function upperCaseCustomerName(){
+   customerName = 'BOB';
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "value2";
+}
